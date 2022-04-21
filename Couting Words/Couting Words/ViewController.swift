228f0d52053dd780034e128f664c60e4023d91df
Junior Fernandes <72802121+waldisonjunior@users.noreply.github.com>
2022-04-21 09:38:27 -0300

    creating the project

---
+++ Couting Words/Couting Words/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Couting Words
+//
+//  Created by Junior Fernandes on 21/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
