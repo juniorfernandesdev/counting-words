e6ea5e49a8229ca0768cb06ee178c908f73f1697
Junior Fernandes <72802121+waldisonjunior@users.noreply.github.com>
2022-04-21 09:46:49 -0300

    creating the file from the first screen to remove the storyboard

---
+++ Couting Words/Couting Words/Scenes/InputSentence/InputSentenceViewController.swift
@@ -0,0 +1,15 @@
+//
+//  InputSentenceViewController.swift
+//  Couting Words
+//
+//  Created by Junior Fernandes on 21/04/22.
+//
+
+import UIKit
+
+class InputSentenceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
